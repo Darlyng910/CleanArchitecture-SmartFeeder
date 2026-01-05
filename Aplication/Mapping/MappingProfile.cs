d1cd45d7a0eba7efd0855ff9095a8af434f46cef
DarlynCG <darlynkelly09@gmail.com>
2026-01-05 18:04:34 -0400

    Implementación del Mapping

---
+++ Aplication/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using Aplication.DTOs;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Mascota, MascotaDTOs>().ReverseMap();
+            CreateMap<Horario, HorarioDTOs>().ReverseMap();
+            CreateMap<Alimentacion, AlimentacionDTOs>().ReverseMap();
+        }
+    }
+}
